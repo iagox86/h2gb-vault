cdc143ac71abff7692f7af12205dc23435e44716
Ron <ron@skullsecurity.net>
2014-09-23 15:24:34 -0700

    Improved the x86 / x64 disassembler: refs + xrefs are now computed automatically

--- arch/x64.rb
+++ arch/x64.rb
@@ -5,10 +5,14 @@
 require 'metasm'
 
 module X64
-  def disassemble_x86(code, base = 0)
+  MANDATORY_JUMPS = [ "jmp" ]
+  OPTIONAL_JUMPS  = [ "jo", "jno", "js", "jns", "je", "jz", "jne", "jnz", "jb", "jnae", "jc", "jnb", "jae", "jnc", "jbe", "jna", "ja", "jnbe", "jl", "jnge", "jge", "jnl", "jle", "jng", "jg", "jnle", "jp", "jpe", "jnp", "jpo", "jcxz", "jecxz" ]
+
+  def disassemble_x64(code, base = 0)
     cpu = Metasm::X86_64.new
     d = Metasm::EncodedData.new(code)
     instructions = []
+    i_by_address = {}
 
     loop do
       start = d.ptr
@@ -26,7 +30,7 @@ module X64
         if(arg.is_a?(Metasm::Expression))
           operands << {
             :type => 'immediate',
-            :value => "%s%s%s" % [arg.lexpr || '', arg.op || '', arg.rexpr || '']
+            :value => ("%s%s%s" % [arg.lexpr || '', arg.op || '', arg.rexpr || '']).to_i()
           }
         elsif(arg.is_a?(Metasm::Ia32::Reg))
           operands << {
@@ -71,11 +75,42 @@ module X64
         :raw    => bytes.unpack("H*").pop.gsub(/(..)(?=.)/, '\1 '),
         :type   => "instruction",
         :instruction => instruction,
-        :refs   => [],
-        :xrefs  => [],
+        :refs => [],
+        :xrefs => [],
       }
 
       instructions << result
+      i_by_address[result[:offset]] = result
+    end
+
+    # Deal with jumps / xrefs / etc (note: this isn't really architecture-specific)
+    0.upto(instructions.length - 1) do |i|
+      instruction = instructions[i]
+
+      refs = []
+      operator = instruction[:instruction][:operator]
+      operand = instruction[:instruction][:operands][0]
+
+      # If it's not a mandatory jump, it references the next address
+      if(MANDATORY_JUMPS.index(operator).nil?)
+        if(!instructions[i+1].nil?)
+          refs << instructions[i+1][:offset]
+        end
+      end
+
+      # If it's a jump of any kind (with an immediate destination), fill in the ref
+      if((MANDATORY_JUMPS.index(operator) || OPTIONAL_JUMPS.index(operator)) && operand[:type] == 'immediate')
+        refs << operand[:value]
+      end
+
+      # Do Xrefs for each of the refs we just found
+      refs.each do |r|
+        if(!i_by_address[r].nil?)
+          i_by_address[r][:xrefs] << instruction[:offset]
+        end
+      end
+
+      instructions[i][:refs] = refs
     end
 
     return instructions
