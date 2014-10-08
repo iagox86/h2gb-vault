05dee9f550fc9743d3e6e2f3de2d5d001251016f
Ron <ron@skullsecurity.net>
2014-10-07 19:11:14 -0700

    Re-wrote the nodes of the memory class to be hashes rather than their own classes

--- memory.rb
+++ memory.rb
@@ -2,56 +2,56 @@
 # By Ron Bowes
 # Created October 6, 2014
 
-class MemoryNode
-  attr_reader :type, :real_addr, :file_addr, :length
-
-  def initialize(type, real_addr, file_addr, length)
-    @type = type
-    @real_addr = real_addr
-    @file_addr = file_addr
-    @length = length
-
-    @xrefs = []
-  end
-
-  def raw(memory)
-    return (memory[@real_addr, @length].map do |c| c.chr end).join
-  end
-
-  def value(memory)
-    return "db 0x%02x" % (memory[@real_addr].ord & 0x0FF)
-  end
-end
-
-class MemoryNodeDword < MemoryNode
-  def initialize(real_addr, file_addr)
-    super("dword", real_addr, file_addr, 4)
-  end
-
-  def value(memory)
-    return "dd 0x%08x" % raw(memory).unpack("I")
-  end
-end
-
-class MemoryNodeWord < MemoryNode
-  def initialize(real_addr, file_addr)
-    super("word", real_addr, file_addr, 2)
-  end
-
-  def value(memory)
-    return "dw 0x%04x" % raw(memory).unpack("S")
-  end
-end
-
-class MemoryNodeByte < MemoryNode
-  def initialize(real_addr, file_addr)
-    super("byte", real_addr, file_addr, 1)
-  end
-
-  def value(memory)
-    return "db 0x%02x" % raw(memory)[0].ord
-  end
-end
+#class MemoryNode
+#  attr_reader :type, :real_addr, :file_addr, :length
+#
+#  def initialize(type, real_addr, file_addr, length)
+#    @type = type
+#    @real_addr = real_addr
+#    @file_addr = file_addr
+#    @length = length
+#
+#    @xrefs = []
+#  end
+#
+#  def raw(memory)
+#    return (memory[@real_addr, @length].map do |c| c.chr end).join
+#  end
+#
+#  def value(memory)
+#    return "db 0x%02x" % (memory[@real_addr].ord & 0x0FF)
+#  end
+#end
+#
+#class MemoryNodeDword < MemoryNode
+#  def initialize(real_addr, file_addr)
+#    super("dword", real_addr, file_addr, 4)
+#  end
+#
+#  def value(memory)
+#    return "dd 0x%08x" % raw(memory).unpack("I")
+#  end
+#end
+#
+#class MemoryNodeWord < MemoryNode
+#  def initialize(real_addr, file_addr)
+#    super("word", real_addr, file_addr, 2)
+#  end
+#
+#  def value(memory)
+#    return "dw 0x%04x" % raw(memory).unpack("S")
+#  end
+#end
+#
+#class MemoryNodeByte < MemoryNode
+#  def initialize(real_addr, file_addr)
+#    super("byte", real_addr, file_addr, 1)
+#  end
+#
+#  def value(memory)
+#    return "db 0x%02x" % raw(memory)[0].ord
+#  end
+#end
 
 class MemorySegment
   attr_reader :name, :real_addr, :file_addr, :data
@@ -102,18 +102,18 @@ class Memory
     @actions = []
   end
 
-  def find_segment(addr)
-    @segments.each_pair do |_, s|
-      if(addr >= s.real_addr && addr < (s.real_addr + s.length))
-        return s
-      end
-    end
-
-    raise SegmentationException
-  end
+#  def find_segment(addr)
+#    @segments.each_pair do |_, s|
+#      if(addr >= s.real_addr && addr < (s.real_addr + s.length))
+#        return s
+#      end
+#    end
+#
+#    raise SegmentationException
+#  end
 
   def remove_node(node, rewindable = true)
-    node.real_addr.upto(node.real_addr + node.length - 1) do |addr|
+    node[:address].upto(node[:address] + node[:length] - 1) do |addr|
       @memory_nodes[addr] = nil
     end
 
@@ -130,26 +130,35 @@ class Memory
     end
   end
 
-  def add_node(node, rewindable = true)
-    # Make sure we're in a valid segment
-    segment = find_segment(node.real_addr)
-
+  def add_node_internal(node, rewindable = true)
     # Make sure there's enough room
-    if(!segment.contains_node?(node))
-      raise SegmentationException
+    node[:address].upto(node[:address]+node[:length] - 1) do |addr|
+      # There's no memory
+      if(@memory_bytes[addr].nil?)
+        raise SegmentationException
+      end
     end
 
     # Make sure the nodes are undefined
-    undefine(node.real_addr, node.length)
+    undefine(node[:address], node[:length])
 
-    # Reserve the memory
-    node.real_addr.upto(node.real_addr + node.length - 1) do |addr|
+    # Save the node to memory
+    node[:address].upto(node[:address]+node[:length] - 1) do |addr|
       @memory_nodes[addr] = node
     end
 
     if(rewindable)
       @actions << { :type => :add, :node => node }
     end
+  end
+
+  def add_node(type, address, length, details, rewindable = true)
+    return add_node_internal({
+      :type    => type,
+      :address => address,
+      :length  => length,
+      :details => details
+    }, rewindable)
   end
 
   def rewind(steps = 1)
@@ -159,7 +168,7 @@ class Memory
       if(action[:type] == :add)
         remove_node(action[:node], false)
       elsif(action[:type] == :remove)
-        add_node(action[:node], false)
+        add_node_internal(action[:node], false)
       else
         puts("Unknown action: #{action[:type]}")
         raise NotImplementedException
@@ -218,7 +227,7 @@ class Memory
         i += 1
       else
         # We're in a node
-        s += "0x%08x %s\n" % [i, @memory_nodes[i].value(@memory_bytes)]
+        s += "0x%08x %s\n" % [i, @memory_nodes[i][:details]]
         i += @memory_nodes[i].length
       end
     end
@@ -226,10 +235,20 @@ class Memory
     return s
   end
 
-  def test()
-    0.upto(@memory_nodes.length - 1) do |i|
-      puts("%x => %s" % [i, @memory_nodes[i]])
-    end
+  def get_bytes_at(addr, length)
+    return (@memory_bytes[addr, length].map do |c| c.chr end).join
+  end
+
+  def get_dword_at(addr)
+    return get_bytes_at(addr, 4).unpack("I")
+  end
+
+  def get_word_at(addr)
+    return get_bytes_at(addr, 2).unpack("S")
+  end
+
+  def get_byte_at(addr)
+    return get_bytes_at(addr, 1).ord
   end
 end
 
@@ -239,15 +258,15 @@ m.mount_segment(MemorySegment.new("s1", 0x1000, 0x0000, "A" * 16))
 m.mount_segment(MemorySegment.new("s2", 0x2000, 0x0000, "B" * 8))
 
 puts("Inserting new node")
-m.add_node(MemoryNodeDword.new(0x1000, 0x0000))
-m.add_node(MemoryNodeWord.new(0x1004, 0x0000))
-m.add_node(MemoryNodeByte.new(0x1008, 0x0000))
+m.add_node('dword', 0x1000, 4, { value: m.get_dword_at(0x1000) })
+m.add_node('word',  0x1004, 2, { value: m.get_word_at(0x1004) })
+m.add_node('byte', 0x1008, 1, { value: m.get_byte_at(0x1008) })
 
 puts(m.to_s)
 
-m.add_node(MemoryNodeDword.new(0x1000, 0x0000))
-m.add_node(MemoryNodeDword.new(0x1004, 0x0000))
-m.add_node(MemoryNodeDword.new(0x1008, 0x0000))
+m.add_node('dword', 0x1000, 4, { value: m.get_dword_at(0x1000) })
+m.add_node('word',  0x1004, 4, { value: m.get_dword_at(0x1004) })
+m.add_node('byte',  0x1008, 4, { value: m.get_dword_at(0x1008) })
 
 puts(m.to_s)
 puts()
